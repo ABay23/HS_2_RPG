21eb6530a1e757179059e8339430753bf62929df
Alejandro Bay <alejandrobay23@gmail.com>
2025-01-13 14:46:45 -0500

    Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region Components
     public Animator _anim {get; private set;}
-
+    #endregion Components
+    #region states
     public PlayerStateMachine stateMachine {get; private set;}
 
     public PlayerIdleState idleState {get; private set;}
 
     public PlayerMoveState moveState {get; private set;}
+    #endregion states
 
     private void Awake() 
     {
