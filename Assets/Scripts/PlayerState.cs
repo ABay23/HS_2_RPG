d260b802151b359dd6a5f556182fc13fed141da0
Alejandro Bay <alejandrobay23@gmail.com>
2025-01-12 19:56:27 -0500

    Update State machine

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -1,8 +1,17 @@
+using UnityEditor.AnimatedValues;
 using UnityEngine;
 
 public class PlayerState
 {
-    protected PlayerStateMachine playerState;
+    protected PlayerStateMachine stateMachine;
     protected Player player;
+    protected string animBoolName;
+
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName )
+    {
+        this.player = _player;
+        this.stateMachine = _stateMachine;
+        this.animBoolName = _animBoolName;
+    }
     
 }
