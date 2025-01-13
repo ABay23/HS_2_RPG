3d13c8c68868d8a38d014b9dcb2db4afb83b5af8
Alejandro Bay <alejandrobay23@gmail.com>
2025-01-13 16:47:06 -0500

    Updated Scripts Fixed Bug

--- Assets/Scripts/States/PlayerJumpState.cs
+++ Assets/Scripts/States/PlayerJumpState.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class PlayerJumpState : PlayerState
 {
+    public PlayerJumpState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
     public override void Enter()
     {
         base.Enter();
