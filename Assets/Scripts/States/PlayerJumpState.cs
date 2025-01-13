4528413d94b628fe3aebca682b16dab3cb6effee
Alejandro Bay <alejandrobay23@gmail.com>
2025-01-13 16:44:39 -0500

    Fixing Compiled Error

---
+++ Assets/Scripts/States/PlayerJumpState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerJumpState : PlayerState
+{
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
