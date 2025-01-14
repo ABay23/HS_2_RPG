7e73e5d42cc5b4e2b977a3e5f97198a688b49945
Alejandro Bay <alejandrobay23@gmail.com>
2025-01-14 12:03:51 -0500

    Changed Air speed

--- Assets/Scripts/States/SuperStates/PlayerAirState.cs
+++ Assets/Scripts/States/SuperStates/PlayerAirState.cs
@@ -19,6 +19,11 @@ public class PlayerAirState : PlayerState
         {
             stateMachine.ChangeState(player.idleState);
         }
+
+        if (player._rb.linearVelocityX != 0)
+        {
+            player.VelocityInput(player._movementSpeed * 0.8f * (_xInput * Time.fixedDeltaTime), player._rb.linearVelocityY);
+        }
     }
 
     public override void Exit()
