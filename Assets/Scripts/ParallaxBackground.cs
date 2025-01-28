76167d792c9f492aa9b17d0dbd549ce54ab8f6ce
Alejandro Bay <alejandrobay23@gmail.com>
2025-01-27 22:48:05 -0500

    Transform Test

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,13 +4,21 @@ public class ParallaxBackground : MonoBehaviour
 {
     private GameObject cam;
     private float _xPossition;
+    private float _distanceMove;
 
     [SerializeField] private float _parallaxEffect;
 
-    private void Update() 
+    private void Start() 
     {
         cam = GameObject.Find("Main Camera");
         _xPossition = transform.position.x;
         
+    }
+
+    private void Update() 
+    {
+        _distanceMove = cam.transform.position.x * _parallaxEffect;
+
+        transform.position = new Vector3(_xPossition +_distanceMove, transform.position.y);
     }
 }
