4a2a58a5b6d2c65ae56508d4cf27f98eba3c9daa
Alejandro Bay <alejandrobay23@gmail.com>
2025-01-27 22:44:46 -0500

    Updating Parallax Script 1

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    
+    private GameObject cam;
+    private float _xPossition;
+
+    [SerializeField] private float _parallaxEffect;
+
+    private void Update() 
+    {
+        cam = GameObject.Find("Main Camera");
+        _xPossition = transform.position.x;
+        
+    }
 }
