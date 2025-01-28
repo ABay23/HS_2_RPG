eb23334b97cd3407a896384b5ba8ac92928e6d5a
Alejandro Bay <alejandrobay23@gmail.com>
2025-01-27 23:12:13 -0500

    Update on endless BG

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -3,22 +3,31 @@ using UnityEngine;
 public class ParallaxBackground : MonoBehaviour
 {
     private GameObject cam;
-    private float _xPossition;
-    private float _distanceMove;
+    private float _xPosition;
+    private float _lenght;
+    private float _distanceMoved;
+    private float _distanceToMove;
 
     [SerializeField] private float _parallaxEffect;
 
     private void Start() 
     {
         cam = GameObject.Find("Main Camera");
-        _xPossition = transform.position.x;
+        _xPosition = transform.position.x;
+        _lenght = GetComponent<SpriteRenderer>().bounds.size.x;
         
     }
 
     private void Update() 
     {
-        _distanceMove = cam.transform.position.x * _parallaxEffect;
+        _distanceToMove = cam.transform.position.x * _parallaxEffect;
+        _distanceMoved= cam.transform.position.x * (1 - _parallaxEffect);
 
-        transform.position = new Vector3(_xPossition +_distanceMove, transform.position.y);
+        transform.position = new Vector3(_xPosition +_distanceToMove, transform.position.y);
+
+        if (_distanceMoved > _xPosition + _lenght)
+            _xPosition = _xPosition + _lenght;
+        else if (_distanceMoved < _xPosition - _lenght)
+            _xPosition = _xPosition - _lenght;
     }
 }
